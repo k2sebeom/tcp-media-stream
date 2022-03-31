1afc5bf3e62754b60cc889685e9e826c887ae8e7
k2sebeom <moses97@gmail.com>
2022-03-31 14:55:41 +0900

    feat: Include supported formats

--- src/util/audio.ts
+++ src/util/audio.ts
@@ -1,5 +1,7 @@
 
 
 export const allowedFormats = [
-    'f32le', 's16le'
+    'f32be', 'f32le', 'f64be', 'f64le', 
+    's16be', 's16le', 's24be', 's24le', 's32be', 's32le', 's8',
+    'u16be', 'u16le', 'u24be', 'u24le', 'u32be', 'u32le', 'u8'
 ]
