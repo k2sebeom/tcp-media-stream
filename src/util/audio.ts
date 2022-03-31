0cd1fd41c78809aff7ba1f6ef2dd406fe9188e35
k2sebeom <moses97@gmail.com>
2022-03-31 14:56:15 +0900

    fix: Fix supported formatS

--- src/util/audio.ts
+++ src/util/audio.ts
@@ -2,6 +2,6 @@
 
 export const allowedFormats = [
     'f32be', 'f32le', 'f64be', 'f64le', 
-    's16be', 's16le', 's24be', 's24le', 's32be', 's32le', 's8',
-    'u16be', 'u16le', 'u24be', 'u24le', 'u32be', 'u32le', 'u8'
+    's16be', 's16le', 's24be', 's24le', 's32be', 's32le',
+    'u16be', 'u16le', 'u24be', 'u24le', 'u32be', 'u32le'
 ]
